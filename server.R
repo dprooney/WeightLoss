b3f657045416261b6981168d0b2114207d263cd8
darraghrooney <dprooney@umich.edu>
2015-03-10 21:16:36 -0700

    Added shiny app, version one

---
+++ server.R
@@ -0,0 +1,92 @@
+library(shiny)
+library(UsingR)
+wldata <- read.csv('data/CPdata.csv')
+
+shinyServer(
+  
+    function(input, output) {
+      
+      output$rawdata <- renderPlot({
+      
+        weights = rep(0,69)
+        weights[1] = 183.2
+        for (j in 1:68){
+          weights[j+1] = weights[j]+wldata[69-j,2]
+        }
+        plot(as.ts(weights),xlab="Day",
+             ylab = "Weight (lbs)", col='blue'  )
+      })
+      
+      output$fit <- renderPlot({
+      
+        N <- 68 - max(input$lossspan + input$lag, input$defspan) + 1
+        procdf <- data.frame(Deficit = rep(0,N),Loss = rep(0,N))
+    
+        for (j in 1:N){
+          procdf[j,1] = mean(wldata[(j+input$lag):(j+input$defspan-1+input$lag),1])
+          procdf[j,2] = mean(wldata[j:(j+input$lossspan-1),2])
+        }
+        
+        mod <- lm(Loss~Deficit, procdf)
+
+        newdef <- seq(min(procdf$Deficit),max(procdf$Deficit),length.out=100)
+        preds <- predict(mod, newdata=data.frame(Deficit=newdef), interval='confidence') 
+        
+        plot(procdf$Deficit,procdf$Loss,type="n",xlab="Caloric Deficit (kcal)",
+             ylab = "Weight Loss (lbs)" ,ylim = c(1.1*min(wldata$Loss),1.1*max(wldata$Loss)) )
+
+        polygon(c(rev(newdef),newdef),c(rev(preds[,3]),preds[,2]),col='grey80',border=NA)
+        points(procdf$Deficit,procdf$Loss,col="blue" )
+        abline(mod,col="red",lwd=3)
+                    })
+      
+      output$result1 <- renderText({
+        
+        N <- 68 - max(input$lossspan + input$lag, input$defspan) + 1
+        procdf <- data.frame(Deficit = rep(0,N),Loss = rep(0,N))
+        
+        for (j in 1:N){
+          procdf[j,1] = mean(wldata[(j+input$lag):(j+input$defspan-1+input$lag),1])
+          procdf[j,2] = mean(wldata[j:(j+input$lossspan-1),2])
+        }
+        
+        mod <- summary(lm(Loss~Deficit, procdf))
+        
+        1/mod$coefficients[2,1]
+        
+      })
+      
+      output$result2 <- renderText({
+        
+        N <- 68 - max(input$lossspan + input$lag, input$defspan) + 1
+        procdf <- data.frame(Deficit = rep(0,N),Loss = rep(0,N))
+        
+        for (j in 1:N){
+          procdf[j,1] = mean(wldata[(j+input$lag):(j+input$defspan-1+input$lag),1])
+          procdf[j,2] = mean(wldata[j:(j+input$lossspan-1),2])
+        }
+        
+        mod <- summary(lm(Loss~Deficit, procdf))
+        
+        mod$coefficients[2,4]
+        
+      })
+
+      output$result3 <- renderText({
+        
+        N <- 68 - max(input$lossspan + input$lag, input$defspan) + 1
+        procdf <- data.frame(Deficit = rep(0,N),Loss = rep(0,N))
+        
+        for (j in 1:N){
+          procdf[j,1] = mean(wldata[(j+input$lag):(j+input$defspan-1+input$lag),1])
+          procdf[j,2] = mean(wldata[j:(j+input$lossspan-1),2])
+        }
+        
+        mod <- summary(lm(Loss~Deficit, procdf))
+        
+        mod$r.squared  
+        
+      })
+            
+  }
+)
---
+++ ui.R
@@ -0,0 +1,56 @@
+shinyUI(pageWithSidebar(
+  headerPanel("Caloric Deficit and Weight Loss",p("what")),
+  sidebarPanel(
+    numericInput("defspan", "Number of days to average caloric deficit:",1,min=1),
+    numericInput("lossspan", "Number of days to average weight loss:",1,min=1),
+    numericInput("lag", "Time lag in days between intake and weight change:",0,min=0)
+  ),
+  mainPanel(
+    tabsetPanel(
+      tabPanel("Documentation",
+        br(),
+        strong("A project for JHU's ", em("Developing Data Products"), " Coursera course"),
+        br(),br(),
+        p("This is a simple app to examine the effect of caloric deficit on weight loss. One
+          commonly cited figure (of questionable validity) is that in order to lose one pound,
+          one must achieve a cumulative caloric deficit of 3500kcal. In order to probe this 
+          figure, one can measure daily caloric intake/expenditure and net change in weight
+          every day. This app plots this data, fits a line, and indicates the confidence
+          intervals in grey."),
+        br(),
+        p("Furthermore, the app lets you plot multi-day averages, to see how
+          the effect changes with time-scale. For example, if the input data is 2, 3 and 1, the
+          first data point would be the average deficit over days 1-2 and the average daily
+          weight loss for days 2-4."),
+        br(), 
+        p("The default data set is a 68-day record of the author's own weight and caloric
+          deficit. Admittedly, this sample size is probably too small to draw firm conclusions.
+          But the data does show a significant effect, with a stronger slope than expected:
+          the linear fit indicates, on a day-to-day basis, only a 1100kcal deficit is needed
+          to lose one pound. This figure increases markedly however when one considers 
+          multi-day averages."),
+        br(),
+        p("The files ",em("ui.R")," and ", em("server.R"), " can be found at ", 
+          a(href = "https://github.com/dprooney/WeightLoss",target = "_blank",
+            "the github repository.")
+        )
+        
+      ),
+      tabPanel("Results",
+        plotOutput('fit'),
+        br(),
+        strong("1/Coefficient (kcal/lbs):"),
+        textOutput('result1'),
+        br(),
+        strong("p-value:"),
+        textOutput('result2'),
+        br(),
+        strong("R-squared:"),
+        textOutput('result3')
+        
+        ),
+      tabPanel("Raw Data",
+        plotOutput('rawdata'))
+    )
+  )
+))
